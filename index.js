fae72b57c252acd309f20398abd45e44b5b9cc47
MrMatvienko <133213584+MrMatvienko@users.noreply.github.com>
2024-05-29 16:22:24 +0300

    slider project

--- index.js
+++ index.js
@@ -19,3 +19,35 @@ document
     }
     sliderLine.style.left = -offset + "px";
   });
+
+let offset2 = 0;
+
+const slider = document.querySelector(".project-gallery-list");
+
+document
+  .querySelector(".button-next-project")
+  .addEventListener("click", function () {
+    offset2 = offset2 + 1237;
+    if (offset2 > 1237) {
+      offset2 = 1237;
+    }
+    slider.style.left = -offset2 + "px";
+  });
+
+let currentIndex = 0;
+const items = document.querySelectorAll(".project-gallery-item");
+
+document
+  .querySelector(".button-next-project")
+  .addEventListener("click", function () {
+    if (currentIndex < items.length - 1) {
+      items[currentIndex].classList.remove("current");
+      items[currentIndex].classList.add("prev");
+      currentIndex++;
+      items[currentIndex].classList.remove("next");
+      items[currentIndex].classList.add("current");
+      if (currentIndex < items.length - 1) {
+        items[currentIndex + 1].classList.add("next");
+      }
+    }
+  });
